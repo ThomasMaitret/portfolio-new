f87eba536bc4e1cf5dd6ef3a1c0865109b668470
Thomas Maitret Richard <thomas.maitret@laposte.net>
2020-10-08 17:24:12 +0200

    better config for tailwind + fix global css

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,10 +1,24 @@
 <script>
   import Nav from "../components/Nav.svelte";
+  import Tailwind from "../components/Tailwind.svelte";
   export let segment;
 </script>
 
+<Tailwind />
 <Nav {segment} />
 
 <main>
   <slot />
 </main>
+
+<style global>
+body {
+  border-top: 10px solid;
+  border-image: linear-gradient(139deg, #7a17fb, #3001ff, #2139c1, #2a4aff) 3;
+}
+
+a:focus {
+  outline: 4px solid var(--theme-color);
+  outline-offset: 0;
+}
+</style>
--- src/server.js
+++ src/server.js
@@ -2,18 +2,15 @@ import sirv from "sirv";
 import polka from "polka";
 import shrinkRay from "shrink-ray-current";
 import * as sapper from "@sapper/server";
-import helmet from "helmet";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
 polka()
   .use(
-    helmet(),
     shrinkRay(),
     sirv("static", {
       dev,
-      maxAge: 31536000,
       etag: true,
     }),
     sapper.middleware()
