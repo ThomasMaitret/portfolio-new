56d14abb3fb3e66b4198fb0529c17d405e10d1c5
Thomas Maitret Richard <thomas.maitret@laposte.net>
2021-04-02 16:45:48 +0200

    new portfolio

---
+++ main.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  setRandomColors();
+
+  import("tsparticles").then(({tsParticles}) => {
+    tsParticles.loadJSON("tsparticles", "./particles.json");
+  });
+});
+
+function setRandomColors() {
+  const rgb = [255, 0, 0];
+
+  setInterval(() => {
+    rgb[0] = Math.round(Math.random() * 255);
+    rgb[1] = Math.round(Math.random() * 255);
+    rgb[2] = Math.round(Math.random() * 255);
+
+    const brightness = Math.round(
+      (parseInt(rgb[0]) * 299 +
+        parseInt(rgb[1]) * 587 +
+        parseInt(rgb[2]) * 114) /
+        1000
+    );
+    const textColor = brightness > 125 ? "black" : "white";
+    const backgroundColor = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
+
+    document.documentElement.style.setProperty(`--text-color`, textColor);
+    document.documentElement.style.setProperty(`--bg-color`, backgroundColor);
+  }, 4000);
+}
---
+++ snowpack.config.js
@@ -0,0 +1,11 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: "es2017",
+  },
+};
